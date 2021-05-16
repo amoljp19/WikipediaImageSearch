aa267870eddc10e84aa0ff777c65a3907ffe234c
amol pawar <amoljp19@gmail.com>
2021-05-16 16:19:33 +0530

    added pagesDao class

---
+++ app/src/main/java/com/softaai/wikipediaimagesearch/data/persistence/PagesDao.kt
@@ -0,0 +1,26 @@
+package com.softaai.wikipediaimagesearch.data.persistence
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.softaai.wikipediaimagesearch.model.Pages
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PagesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addPages(pages: Pages)
+
+    @Query("DELETE FROM ${Pages.TABLE_NAME}")
+    suspend fun deletePages()
+
+//    @Query("SELECT * FROM ${Pages.TABLE_NAME} WHERE ID = :pageId")
+//    fun getPageById(pageId: Int): Flow<Pages>
+
+
+    @Query("SELECT * FROM ${Pages.TABLE_NAME}")
+    fun getAllPages(): Flow<Pages>
+
+}
